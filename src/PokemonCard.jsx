f791e0b2f7130f55a7083a5c7e6486abc2b65f16
VincentRssx <Vincent.rssx59@gmail.com>
2023-10-10 10:05:37 +0200

    fonction pokemonList fonctionnel

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,10 +1,23 @@
 
-function PokemonCard(){
-    return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt ="Bulbasaur"></img>
-    <figcaption>Bulbasaur</figcaption>
 
-</figure>
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[0];
+
+
+  return <figure>{pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}</figure>
+
 }
 
 export default PokemonCard;
+
